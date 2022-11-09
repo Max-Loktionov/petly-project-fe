9be44858194b2fabb96a3438de14f3ca54014d09
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-11-09 23:06:37 +0200

    styles for header

---
+++ src/components/Logo/Logo.styled.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const LogoText = styled.p`
+  font-family: "Poppins";
+  color: #111111;
+  font-weight: 700;
+  font-size: 28px;
+  line-height: 1.5;
+  letter-spacing: 0.07em;
+  @media (min-width: 768px) {
+    font-size: 32px;
+  }
+`;
+
+export const AccentLogo = styled.span`
+  color: #f59256;
+`;
---
+++ src/components/Nav/Nav.styled.jsx
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const StyledNavLink = styled(NavLink)`
+  font-weight: 500;
+  font-size: 32px;
+  line-height: 1.37;
+  letter-spacing: 0.04em;
+  color: #181c27;
+`;
