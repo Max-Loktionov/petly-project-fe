8786fff8bd85de69bc2d3063b71127a7182b77f5
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-11-09 23:06:07 +0200

    header components
    1. Logo
    2. Navigation

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import { Logo } from "components/Logo/Logo";
+import { Navigation } from "components/Navigation/Navigation";
+import { Container } from "./Header.styled";
+
+export const Header = () => {
+  return (
+    <header>
+      <Container>
+        <Logo />
+        <Navigation />
+      </Container>
+    </header>
+  );
+};
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import { AccentLogo, LogoText } from "./Logo.styled";
+
+export const Logo = () => {
+  return (
+    <>
+      <LogoText>
+        pe<AccentLogo>t</AccentLogo>ly
+      </LogoText>
+    </>
+  );
+};
