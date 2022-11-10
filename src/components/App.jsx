aa87e6349ff03ee0c086387abb3dad903bf02f1a
SerhiiDubovets <Sergio.d@bigmir.net>
2022-11-10 12:12:57 +0200

    add style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
+import GlobalStyle from "styles/GlobalStyle";
+import theme from "styles/theme";
 
 const SharedLayout = lazy(() => import("./SharedLayout"));
 const Home = lazy(() => import("../pages/Home"));
@@ -7,15 +11,18 @@ const NotFound = lazy(() => import("../pages/NotFound"));
 
 export const App = () => {
   return (
-    <BrowserRouter basename="/petly-project-fe">
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter basename="/petly-project-fe">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<SharedLayout />}>
+              <Route index element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,62 @@
+import "modern-normalize";
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+
+
+ul,
+ol {
+  padding: 0;
+  margin: 0;
+}
+
+body,
+h1,
+h2,
+h3,
+h4,
+p,
+li {
+  margin: 0;
+  padding: 0;
+}
+
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+
+a {
+  text-decoration: none;
+  font-style: normal;
+  color: inherit;
+  display: block;
+}
+
+code {
+  font-family:  source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+`;
+
+export default GlobalStyle;
