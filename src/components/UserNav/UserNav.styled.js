20efbc8f4b4a7d7555e4c87edb393d2068e632fa
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-11-11 20:17:29 +0200

    styles for UserNav

--- src/components/UserNav/UserNav.styled.js
+++ src/components/UserNav/UserNav.styled.js
@@ -2,7 +2,14 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const StyledNavLink = styled(NavLink)`
-  @media (min-width: 1280px) {
+  font-weight: ${p => p.theme.fontWeights.heading};
+  font-size: 14px;
+  line-height: 1.35;
+  letter-spacing: 0.04em;
+  @media ${p => p.theme.media.tablet} {
+    font-size: 16px;
+  }
+  @media ${p => p.theme.media.desktop} {
     display: inline-block;
     margin-left: auto;
   } ;
