98511fcaabc0b8cda3ce56f615f58214f9091679
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2022-11-14 09:02:04 +0200

    add NoticePage

---
+++ src/pages/NoticesPage/NoticesPage.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-family: "Manrope";
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 33px;
+  /* identical to box height */
+
+  text-align: center;
+
+  margin-bottom: 28px;
+
+  color: #111111;
+`;
