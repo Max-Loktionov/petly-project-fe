27dbdcda7a1f4975502471215914b49a2f37edd4
SerhiiDubovets <Sergio.d@bigmir.net>
2022-11-15 17:14:49 +0200

    фіксики

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,13 +10,11 @@ import NoticesPage from "pages/NoticesPage/NoticesPage";
 const SharedLayout = lazy(() => import("./SharedLayout"));
 const Home = lazy(() => import("../pages/Home"));
 const NotFound = lazy(() => import("../pages/NotFound"));
-<<<<<<< HEAD
 
 const UserPage = lazy(() => import("../pages/UserPage"));
 // const RegisterPage = lazy(() => import("../pages/RegisterPage"));
-=======
+
 const RegisterPage = lazy(() => import("../pages/AuthPages/RegisterPage"));
->>>>>>> d79bcbf134ec696695ce12e955d0c848ed47875e
 
 export const App = () => {
   return (
@@ -27,7 +25,7 @@ export const App = () => {
           <Routes>
             <Route path="/" element={<SharedLayout />}>
               <Route index element={<Home />} />
-              <Route path="profile" element={<UserPage />} />
+              <Route path="user" element={<UserPage />} />
               <Route path="register" element={<RegisterPage />} />
               <Route path="notices" element={<NoticesPage />} />
               <Route path="*" element={<NotFound />} />
