682aa5fe43baeb80ad924fc0d300793f2976dfc8
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-11-16 21:49:40 +0200

    Open/close modal

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,11 +1,31 @@
+import Button from "components/Button";
 import Modal from "components/Modal/Modal";
 import ModalAddsPet from "components/ModalAddsPet";
+import { useState } from "react";
 
 const UserPage = () => {
+  const [isOpenAddPetModal, setIsOpenAddPetModal] = useState(false);
+
+  const closeAddPetModal = e => {
+    if (e) {
+      setIsOpenAddPetModal(false);
+    }
+  };
+  const openAddPetModal = e => {
+    if (e) {
+      setIsOpenAddPetModal(true);
+    }
+  };
   return (
-    <Modal>
-      <ModalAddsPet />
-    </Modal>
+    <>
+      <Button onClick={openAddPetModal}>Add pet</Button>
+
+      {isOpenAddPetModal && (
+        <Modal onClose={closeAddPetModal}>
+          <ModalAddsPet />
+        </Modal>
+      )}
+    </>
   );
 };
 
