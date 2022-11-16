1410ce7e7e76e45d812c2e156483625e394c6420
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-11-16 21:52:36 +0200

    .

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,7 @@
 import { LearnButton, Container } from "./Home.styled";
 import Button from "components/Button";
-import {
-  useAddPetMutation,
-  useDeletePetMutation,
-  useGetUserQuery,
-  useUpdateUserAvatarMutation,
-} from "redux/userApi";
 
 const Home = () => {
-  const { data, error } = useGetUserQuery();
-  // console.log("data", data);
-  // console.log("error", error);
-
-  const [addPet] = useAddPetMutation();
-  const newPet = { name: "vfv", age: 1 };
-  const [deletePet, { isSuccess, isLoading }] = useDeletePetMutation();
-  // console.log("isSuccess", isSuccess);
-  // console.log("isLoading", isLoading);
-  const [updateUserAvatar] = useUpdateUserAvatarMutation();
-
   return (
     <>
       <Container>
@@ -26,11 +9,6 @@ const Home = () => {
         <Button active>sell</Button>
         <Button>lost/found</Button>
         <LearnButton disabled>Learn more</LearnButton>
-        <Button onClick={() => deletePet(10)}>deletePet</Button>
-        <Button onClick={() => addPet(newPet)}>addPet</Button>
-        <Button onClick={() => updateUserAvatar(newPet)}>
-          updateUserAvatar
-        </Button>
       </Container>
     </>
   );
--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -2,9 +2,11 @@ import Button from "components/Button";
 import Modal from "components/Modal/Modal";
 import ModalAddsPet from "components/ModalAddsPet";
 import { useState } from "react";
+import { useGetUserQuery } from "redux/userApi";
 
 const UserPage = () => {
   const [isOpenAddPetModal, setIsOpenAddPetModal] = useState(false);
+  const { data, error } = useGetUserQuery();
 
   const closeAddPetModal = e => {
     if (e) {
