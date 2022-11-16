5e9ba2804728411dbf47e960db64f364b87976c7
Ann Sarviro <annka_nuha@ai.ua>
2022-11-16 14:27:50 +0200

    fix default img

--- src/components/OurFriendsCard/FriendCard.js
+++ src/components/OurFriendsCard/FriendCard.js
@@ -1,17 +1,17 @@
 // import PropTypes from "prop-types";
 import { Box, List, Img, Elem, Picture, Title, Time } from "./FriendCard.styled";
 import WorkTime from "./WorkTime";
-import image from "./cat.png";
-const BASE_URL = "https://petly-be.herokuapp.com/friends";
+import image from "../../img/defaultLogo.jpg";
+const BASE_URL = "https://petly-be.herokuapp.com/friends/";
 
 const FriendCard = friend => {
   const { title, address, email, phone, workDays, imageUrl } = friend.friend;
-  console.log(friend);
+
   return (
     <>
       <Title>{title}</Title>
       <Box>
-        <Picture>{{ imageUrl } ? <Img src={BASE_URL + imageUrl} alt={title} /> : <Img src="./cat.png" alt={title} />}</Picture>
+        <Picture>{imageUrl ? <Img src={BASE_URL + imageUrl} alt={title} /> : <Img src={image} alt={title} />}</Picture>
         <List>
           <Time>
             Time:
