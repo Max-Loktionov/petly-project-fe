fb2f910a40d5d1514975ce6bd166a92a9e4489d5
Ann Sarviro <annka_nuha@ai.ua>
2022-11-17 23:41:02 +0200

    fix

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -62,6 +62,7 @@ const theme = {
   },
   media: {
     mobile: "(min-width: 320px)",
+    mobileM: "(max-width: 767px)",
     tablet: "(min-width: 768px)",
     desktop: "(min-width: 1280px)",
   },
