ab7cac2ad8cc165b0c637fc8e4e9e68ea5ffb6f7
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-11-17 22:43:42 +0200

    Fix open ModalAddPet

--- src/components/AddPetBtn/AddPetBtn.jsx
+++ src/components/AddPetBtn/AddPetBtn.jsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
 import { AddPet } from "./AddPetBtn.styled";
 
-const AddPetBtn = ({ disabled = false, openAddPetModal, ...props }) => {
-  const [showModal, setShowModal] = useState(false);
-
-  const toggleModal = () => {
-    setShowModal(!showModal);
-  };
+const AddPetBtn = ({ disabled = false, onOpenModal: openAddPetModal, ...props }) => {
   return (
     <>
       <AddPet active disabled={disabled} onClick={openAddPetModal} {...props}>
         +
       </AddPet>
-      {showModal && (
-        <div onClose={toggleModal}>
-          <p>modal</p>
-          <button onClick={toggleModal}>close</button>
-        </div>
-      )}
     </>
   );
 };
--- src/components/PetsData/PetsData.jsx
+++ src/components/PetsData/PetsData.jsx
@@ -2,37 +2,16 @@ import AddPetBtn from "components/AddPetBtn";
 
 import { TitlePet, BoxBtn, BoxTitlePet, BoxPet, TitleBtn } from "./PetsData.styled";
 import PetsList from "components/PetsList";
-import Modal from "components/Modal/Modal";
-import ModalAddsPet from "components/ModalAddsPet";
-import { useState } from "react";
 
-const PetsData = () => {
-  const [isOpenAddPetModal, setIsOpenAddPetModal] = useState(false);
-
-  const closeAddPetModal = e => {
-    if (e) {
-      setIsOpenAddPetModal(false);
-    }
-  };
-  const openAddPetModal = e => {
-    if (e) {
-      setIsOpenAddPetModal(true);
-    }
-  };
-
+const PetsData = ({ onOpenModal }) => {
   return (
     <>
-      {isOpenAddPetModal && (
-        <Modal onClose={closeAddPetModal}>
-          <ModalAddsPet onClose={closeAddPetModal} />
-        </Modal>
-      )}
       <BoxPet>
         <BoxTitlePet>
           <TitlePet>My pets:</TitlePet>
           <BoxBtn>
             <TitleBtn>Add pet</TitleBtn>
-            <AddPetBtn openAddPetModal={openAddPetModal} disabled={false} />
+            <AddPetBtn onOpenModal={onOpenModal} />
           </BoxBtn>
         </BoxTitlePet>
         <PetsList />
