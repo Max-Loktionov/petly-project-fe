f622bff1da7785f8d50ec3bfb57eb956f9e1c8b2
Yana Mironyak <yanika.mironyak@gmail.com>
2022-11-18 02:28:46 +0200

    add buttons favorite/myAds

--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilterCategories } from 'redux/filterCategoriesSlice';
 import { Button } from './NoticesCategoriesNav.styled';
@@ -6,15 +7,28 @@ export const NoticesCategoriesNav = () => {
     const dispatch = useDispatch();
     const category = useSelector(state => state.categories.category);
     console.log(category);
+    const token = useSelector(state => state.auth.token);
+
+    const [isAuthorized, setIsAuthorized] = useState(false);
+
+    useEffect(() => {
+        if (token) {
+            setIsAuthorized(true);
+        }
+    }, [setIsAuthorized, token]);
 
     const handleClick = (filteredCategory) => { 
         dispatch(setFilterCategories(filteredCategory))
     }
     return (
         <>
-            <Button onClick={() => handleClick('lost')}>lost/found</Button>
-            <Button onClick={() => handleClick('good')}>In good hands</Button>
+            <Button onClick={() => handleClick('lost_found')}>lost/found</Button>
+            <Button onClick={() => handleClick('in_good_hands')}>In good hands</Button>
             <Button onClick={() => handleClick('sell')}>sell</Button>
+            {isAuthorized && (<div>
+                <Button onClick={() => handleClick('favorite')}>Favorite ads</Button>
+                <Button onClick={() => handleClick('own')}>My ads</Button>
+            </div>)}
         </>
     )
 }
