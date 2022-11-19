5c4f5dc9ae26e03e56a8a8faf61d8a75f83aff35
max-loktionov <studreno@gmail.com>
2022-11-19 10:38:26 +0200

    add modal, del notice

--- src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
 import { useDeleteNoticeMutation } from "redux/noticesApi";
-import { Item, ImageThumb, Image, Category, BtnFavorite, Title,ContainerDescription, Table, ButtonMore } from "./NoticeCategoryItem.styled";
-import unlike from "img/unlike.svg"
+import {
+  Item,
+  ImageThumb,
+  Image,
+  Category,
+  BtnFavorite,
+  Title,
+  ContainerDescription,
+  Table,
+  ButtonMore,
+} from "./NoticeCategoryItem.styled";
+import unlike from "img/unlike.svg";
 
-const NoticeCategoryItem = ({ _id, name, title, birthday, breed, male, location, price, image, onModalOpen }) => { 
-    const [isFavorite, setFavorite] = useState(false);
-    const [deleteNotice, { isLoading: isDeleting }] = useDeleteNoticeMutation();
+const NoticeCategoryItem = ({ _id, name, title, birthday, breed, male, location, price, image, onModalOpen }) => {
+  const [isFavorite, setFavorite] = useState(false);
+  const [deleteNotice, { isLoading: isDeleting }] = useDeleteNoticeMutation();
 
-    const currentAge = (date) => {
-        let today = new Date();
+  const currentAge = date => {
+    let today = new Date();
     let birthDate = new Date(date);
     let age = today.getFullYear() - birthDate.getFullYear();
 
@@ -16,53 +26,60 @@ const NoticeCategoryItem = ({ _id, name, title, birthday, breed, male, location,
     let d = today.getDay() - birthDate.getDay();
 
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
+      age--;
     }
-    if ( age === 0 ) {
-        m = 12 + m;
-        if (d < 0 || (d === 0 && today.getDate() < birthDate.getDate())) {
-            m--;
-        }
-    }
-
-    return age ? age + ' year' : m + ' month';
+    if (age === 0) {
+      m = 12 + m;
+      if (d < 0 || (d === 0 && today.getDate() < birthDate.getDate())) {
+        m--;
+      }
     }
 
-    return (
-        <Item>
-            <ImageThumb>
-                <Image src="https://cdn.pixabay.com/photo/2021/10/27/19/09/cat-6748193_960_720.jpg" alt={title}></Image>
-                <Category>Sell</Category>
-                <BtnFavorite><img src={unlike} alt="unlike"/></BtnFavorite>
-            </ImageThumb> 
-            <div>
-                <Title>{title}</Title>                
-                <ContainerDescription>    
-                    <Table>
-                        <tbody>
-                            <tr>
-                                <td>Breed:</td>
-                                <td>{breed}</td>
-                            </tr> 
-                            <tr>
-                                <td>Place:</td>
-                                <td>{location}</td>
-                            </tr>
-                            <tr>
-                                <td>Age:</td>
-                                <td>{currentAge(birthday)}</td>
-                            </tr>
-                            <tr>
-                                <td>Price:</td>
-                                <td>{price}</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </ContainerDescription> 
-            </div>    
-            <ButtonMore active="true" type="button" onClick={() => onModalOpen(_id)}>Learn more</ButtonMore> 
-        </Item>
-    )
-}
+    return age ? age + " year" : m + " month";
+  };
+
+  return (
+    <Item>
+      <ImageThumb>
+        <Image src="https://cdn.pixabay.com/photo/2021/10/27/19/09/cat-6748193_960_720.jpg" alt={title}></Image>
+        <Category>Sell</Category>
+        <BtnFavorite type="button" onClick={() => console.log("isFavorite")}>
+          <img src={unlike} alt="unlike" />
+        </BtnFavorite>
+      </ImageThumb>
+      <div>
+        <Title>{title}</Title>
+        <ContainerDescription>
+          <Table>
+            <tbody>
+              <tr>
+                <td>Breed:</td>
+                <td>{breed}</td>
+              </tr>
+              <tr>
+                <td>Place:</td>
+                <td>{location}</td>
+              </tr>
+              <tr>
+                <td>Age:</td>
+                <td>{currentAge(birthday)}</td>
+              </tr>
+              <tr>
+                <td>Price:</td>
+                <td>{price}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </ContainerDescription>
+      </div>
+      <ButtonMore active="true" type="button" onClick={() => onModalOpen(_id)}>
+        Learn more
+      </ButtonMore>
+      <button type="button" disabled={isDeleting} onClick={() => deleteNotice(_id)}>
+        Delete
+      </button>
+    </Item>
+  );
+};
 
 export default NoticeCategoryItem;
