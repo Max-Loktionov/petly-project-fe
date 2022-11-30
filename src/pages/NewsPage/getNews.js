86681d7cd87169d68d1f5a9f10ad97a374597bc4
max-loctionov <studreno@gmail.com>
2022-11-30 13:21:01 +0200

    fix change key of the env change base url of the links

--- src/pages/NewsPage/getNews.js
+++ src/pages/NewsPage/getNews.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const { REACT_APP_BASE_URL } = process.env;
+const SERVER_NAME = process.env.REACT_APP_SITE_URL;
 
-axios.defaults.baseURL = REACT_APP_BASE_URL;
+axios.defaults.baseURL = SERVER_NAME;
 
 const getNews = async () => {
   try {
-    const response = await axios.get("news");
+    const response = await axios.get("/news");
     return response.data;
   } catch (error) {
     console.log(error.message);
