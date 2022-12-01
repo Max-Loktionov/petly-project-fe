7781967cfd7bea6ca3acfd7bf4c4a7498413d7c4
max-loctionov <studreno@gmail.com>
2022-12-01 11:51:23 +0200

    fix homepage for git

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/petly-project-fe/">
+      <BrowserRouter basename="/petly-project-fe">
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
